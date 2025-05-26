e5ad39a9582fd7349b24d26d602635e6b4eaf05e
superrabbithero <huangding1996@gmail.com>
2025-05-26 22:54:02 +0800

    修改登录压测方式改为顺序执行

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, SequentialTaskSet
 import csv
 import os
 from queue import Queue
@@ -18,29 +18,38 @@ class AccountLoader:
 
 account_loader = AccountLoader()
 
-class LoginUser(HttpUser):
-    wait_time = between(1, 3)  # 用户等待时间1-5秒
-    host = "https://xuece-xqdsj-stagingtest1.unisolution.cn"
-    
+class UserLoginBehavior(SequentialTaskSet):
+    wait_time = between(1,3)
+
     def on_start(self):
-        # 每个用户获取唯一账号
         self.account = account_loader.get_account()
         print(f"用户使用账号 {self.account['username']} 登录")
+        self.auth_token = None
+        self.school_id = None
+        self.login()
 
-    @task
     def login(self):
         params = {
             "username":self.account['username'],
-            "encryptpwd":'c34dd995a8132605764a9347dae6e8ca',
+            "encryptpwd":'c34dd995a8132605764a9347dae6e8ca', #测试数据统一密码
             "clienttype":"BROWSER",
             "clientversion":"1.30.6",
             "systemversion":"chrome136.0.0.0"
         }
         # self.client.get("/login", params=params)
         with self.client.get("/api/usercenter/nnauth/user/login", params=params, catch_response=True) as response:
-            print(f"响应状态码: {response.status_code}")
-            print(f"响应内容: {response.text}")
+            # print(f"响应状态码: {response.status_code}")
+            # print(f"响应内容: {response.text}")
             if response.status_code == 200:
+                school_id = response_data["data"]["user"]["schoolId"]
+                auth_token = response_data["data"]["authtoken"]
+
+                self.auth_token = auth_token
+                self.school_id = school_id
+
                 response.success()
             else:
                 response.failure(f"Status code: {response.status_code}")
+
+    @task
+
